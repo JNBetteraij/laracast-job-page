600ac09297256d65f2ceb5e00ce72b5c1001c439
JNBetteraij <JNBetteray@gmail.com>
2024-10-14 13:56:48 +0200

    Updating title in job cards to a link

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -8,7 +8,11 @@
     <div class="flex-1 flex flex-col">
         <a href="#" class="self-start text-sm text-gray-400 transition-colors duration-300">{{ $job->employer->name }}</a>
 
-        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800">{{ $job->title }}</h3>
+        <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">
+            <a href="{{ $job->url }}">
+                {{ $job->title }}
+            </a>
+        </h3>
 
         <p class="text-sm text-gray-400 mt-auto">{{ $job->schedule }} - From {{ $job->salary }}</p>
     </div>
